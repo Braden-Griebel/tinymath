fcc3895c8379d75a3f1cc019675e8f8b285e46ca
Braden Griebel <bgriebel@uw.edu>
2025-12-16 12:56:31 -0800

    build(test): Added tests for tinyrand
    Added unit tests for tinyrand generating integers and doubles

--- test/test_tinyrand.c
+++ test/test_tinyrand.c
@@ -2,19 +2,69 @@
 
 #include "tinyrand.h"
 
-// Test seeding the random number generator
-static MunitResult test_seed(const MunitParameter params[], void *data) {
+typedef struct {
+  uint64_t seed;
+} tinyrand_test_data;
+
+static tinyrand_test_data TEST_DATA;
+
+// Function for setting up seed for the tests
+static void *tinyrand_test_setup(const MunitParameter params[], void *data) {
   // Get a random seed for testing
   int64_t seedr = (int64_t)munit_rand_uint32();
   int64_t seedl = (int64_t)munit_rand_uint32();
   uint64_t seed = (seedl << 32) | seedr;
+  tinyrand_test_data test_data = {.seed = seed};
+  TEST_DATA = test_data;
+
+  return (void *)&TEST_DATA;
+}
+
+// Test seeding the random number generator
+static MunitResult test_seed(const MunitParameter params[], void *data) {
+  tinyrand_test_data *test_data = (tinyrand_test_data *)data;
 
   // Create two seeded RNGs
-  tinyrand_rng rng1 = new_rng_seeded(seed);
-  tinyrand_rng rng2 = new_rng_seeded(seed);
+  tinyrand_rng rng1 = tinyrand_new_rng_seeded(test_data->seed);
+  tinyrand_rng rng2 = tinyrand_new_rng_seeded(test_data->seed);
 
   // Test that the first returned random number is the same between the 2
-  munit_assert(tinyrand_next_uint64(&rng1) == tinyrand_next_uint64(&rng2));
+  munit_assert_uint64(tinyrand_next_uint64(&rng1), ==,
+                      tinyrand_next_uint64(&rng2));
+
+  return MUNIT_OK;
+}
+
+// Test generating random uint values
+static MunitResult test_gen_uint64(const MunitParameter params[], void *data) {
+  tinyrand_test_data *test_data = (tinyrand_test_data *)data;
+
+  tinyrand_rng test_rng = tinyrand_new_rng_seeded(test_data->seed);
+
+  uint64_t first_rand_num = tinyrand_next_uint64(&test_rng);
+  uint64_t second_rand_num = tinyrand_next_uint64(&test_rng);
+
+  munit_assert_uint64(first_rand_num, !=, second_rand_num);
+
+  return MUNIT_OK;
+}
+
+// Test generating random double values
+static MunitResult test_gen_double(const MunitParameter params[], void *data) {
+  tinyrand_test_data *test_data = (tinyrand_test_data *)data;
+
+  tinyrand_rng test_rng = tinyrand_new_rng_seeded(test_data->seed);
+
+  double first_rand_num = tinyrand_next_double(&test_rng);
+  double second_rand_num = tinyrand_next_double(&test_rng);
+
+  // Test that the two random numbers are not equal
+  munit_assert_double(first_rand_num, !=, second_rand_num);
+  // Test that they are both between 0 and 1
+  munit_assert_double(first_rand_num, >=, 0);
+  munit_assert_double(first_rand_num, <=, 1);
+  munit_assert_double(second_rand_num, >=, 0);
+  munit_assert_double(second_rand_num, <=, 1);
 
   return MUNIT_OK;
 }
@@ -23,10 +73,23 @@ static MunitResult test_seed(const MunitParameter params[], void *data) {
 static MunitTest tinyrand_suite_tests[] = {
     {.name = (char *)"Testing RNG Seeding",
      .test = test_seed,
-     .setup = NULL,
+     .setup = tinyrand_test_setup,
      .tear_down = NULL,
      .options = MUNIT_TEST_OPTION_NONE,
-     .parameters = NULL}};
+     .parameters = NULL},
+    {.name = (char *)"Testing RNG Generation (uint64)",
+     .test = test_gen_uint64,
+     .setup = tinyrand_test_setup,
+     .tear_down = NULL,
+     .options = MUNIT_TEST_OPTION_NONE,
+     .parameters = NULL},
+    {.name = (char *)"Testing RNG Generation (double)",
+     .test = test_gen_double,
+     .setup = tinyrand_test_setup,
+     .tear_down = NULL,
+     .options = MUNIT_TEST_OPTION_NONE,
+     .parameters = NULL},
+};
 
 // Create the test suite
 static const MunitSuite tinyrand_suite = {.prefix = (char *)"Tinyrand Tests",
